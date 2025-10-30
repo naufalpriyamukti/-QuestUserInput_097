1389412f99c3683e40b50831d50fbb0ec8bc2536
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-10-30 11:39:58 +0700

    //Variabel-varibel untuk mengingat nilai masukan dari keyboard

--- app/src/main/java/com/example/tugas5/TeksEdit.kt
+++ app/src/main/java/com/example/tugas5/TeksEdit.kt
@@ -9,11 +9,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tugas5.ui.theme.Tugas5Theme
 
 @Composable
 fun FormDataDiri(){
+    //Variabel-varibel untuk mengingat nilai masukan dari keyboard
+    var textNama by remerber { mutableStateOf(value = " ") }
+    var textAlamat by remerber { mutableStateOf(value = " ") }
+    var textJK by remerber { mutableStateOf(value = " ") }
+
 
 }
